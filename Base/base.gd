a6454755176ad433fe1c4d35139b4f2643f293fe
Bzparkes <bzparkes@yahoo.com.au>
2025-11-17 22:37:00 +1000

    added in logic to decrement health and restart the game when the base takes damage

--- Base/base.gd
+++ Base/base.gd
@@ -2,12 +2,20 @@ extends Node3D
 
 @export var maximum_health: int = 5
 
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		print("health was changed")
+		label_3d.text = str(current_health)
+		if current_health < 1:
+			get_tree().reload_current_scene()
+		
 @onready var label_3d: Label3D = $Label3D
 
 func _ready() -> void:
-	label_3d.text = str(maximum_health)
+	current_health = maximum_health
 
 func take_damage() -> void:
 	print("damage taken")
-	maximum_health -= 1
-	label_3d.text = str(maximum_health)
+	current_health -= 1
+	
