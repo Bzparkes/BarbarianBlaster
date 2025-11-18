19b4ae5d3fc97f549213feb01072539a0ae18192
Bzparkes <bzparkes@yahoo.com.au>
2025-11-18 13:39:06 +1000

    Added in health text and colour modulation to base

--- Base/base.gd
+++ Base/base.gd
@@ -1,12 +1,15 @@
 extends Node3D
 
-@export var maximum_health: int = 5
+@export var maximum_health: int = 5 
 
 var current_health: int:
 	set(health_in):
 		current_health = health_in
 		print("health was changed")
-		label_3d.text = str(current_health)
+		label_3d.text = str(current_health) + "/" + str(maximum_health)
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		label_3d.modulate = red.lerp(white, float(current_health) / float(maximum_health))
 		if current_health < 1:
 			get_tree().reload_current_scene()
 		
@@ -14,6 +17,9 @@ var current_health: int:
 
 func _ready() -> void:
 	current_health = maximum_health
+	var my_name: String = "Byron"
+	var my_number: int = 41
+	print(my_name + " - " + str(my_number)) 
 
 func take_damage() -> void:
 	print("damage taken")
