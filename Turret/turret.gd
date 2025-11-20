5510e3502f0775e1b58aa60b14992f8d6d479eb3
Bzparkes <bzparkes@yahoo.com.au>
2025-11-20 16:45:31 +1000

    Added in timing for the projectiles to fire and disappear at certain intervals

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @export var projectile: PackedScene
+@onready var barrel: MeshInstance3D = $TurretBase/TurretTop/BarrelTop/Barrel
 
-func _ready() -> void:
+
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = barrel.global_position
