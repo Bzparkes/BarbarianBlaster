3e80be91f0af58d3f735afebc5b70ece5bf3ef8a
Bzparkes <bzparkes@yahoo.com.au>
2025-11-25 18:35:17 +1000

    Added curves for scaling enemy health, as well as ensuring the enemies stop spawning after 30 seconds and disappear when they reach the base

--- Base/base.gd
+++ Base/base.gd
@@ -1,11 +1,10 @@
 extends Node3D
 
-@export var maximum_health: int = 5 
+@export var maximum_health: int
 
 var current_health: int:
 	set(health_in):
 		current_health = health_in
-		print("health was changed")
 		label_3d.text = str(current_health) + "/" + str(maximum_health)
 		var red: Color = Color.RED
 		var white: Color = Color.WHITE
@@ -17,11 +16,7 @@ var current_health: int:
 
 func _ready() -> void:
 	current_health = maximum_health
-	var my_name: String = "Byron"
-	var my_number: int = 41
-	print(my_name + " - " + str(my_number)) 
-
+	
 func take_damage() -> void:
-	print("damage taken")
 	current_health -= 1
 	
